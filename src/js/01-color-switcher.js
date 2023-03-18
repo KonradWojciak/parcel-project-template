c935a0dcaa8f313f626eb5f126423029f2a61987
KonradWojciak <konradwojciak@gmail.com>
2023-03-18 17:12:11 +0100

    ferst mod

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,49 +1,39 @@
+const butSwitchStart = document.querySelector('button[data-start]');
+const butSwitchStop = document.querySelector('button[data-stop]');
+const bodyColor = document.querySelector('body');
 
-async function buttonsColorChange(){
-
-const butSwitchStart = document.querySelector("button[data-start]");
-const butSwitchStop = document.querySelector("button[data-stop]");
-const bodyColor = document.querySelector("body");
-
-let intervalId 
+let intervalId;
 
 async function getRandomHexColor() {
-    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-  }
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
-async function colorChange() { 
- bodyColor.style.backgroundColor = getRandomHexColor()
+async function colorChange() {
+  bodyColor.style.backgroundColor = getRandomHexColor();
+}
 
-};
-
-async  function startColorSwitch() {
-    const intervalId = setInterval(colorChange, 1000)
-    butSwitchStop.disabled = false, 
-    butSwitchStart.disabled = true,
+async function startColorSwitch() {
+  intervalId = setInterval(colorChange, 1000);
+  butSwitchStop.disabled = false;
+  butSwitchStart.disabled = true;
 }
 
 async function stopColorSwitch(intervalId) {
-    clearInterval(intervalId)
+  clearInterval(intervalId);
 }
 
-
-async function butSwitchStart.addEventListener("click", async () =>{
-try{
- startColorSwitch();
-} catch (error){
-    console.log(error.messege)
-}
-
+butSwitchStart.addEventListener('click', async () => {
+  try {
+    startColorSwitch();
+  } catch (error) {
+    console.log(error.messege);
+  }
 });
 
-colorSwitchStop.addEventListener("click", async () =>{
-try{ 
-    stopColorSwitch() 
-    
-    } catch (error){
-        console.log(error.messege)
-    }
-    
-    });
-}
-buttonsColorChange()
+butSwitchStop.addEventListener('click', async () => {
+  try {
+    stopColorSwitch();
+  } catch (error) {
+    console.log(error.messege);
+  }
+});
